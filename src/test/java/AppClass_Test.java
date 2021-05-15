05428e358320ae57aca78e1bc71f3d607309c282
ramduvvuri <ram.duvvuri@gmail.com>
2021-05-15 08:24:27 +0100

    Jira 1234 - Adding new project to repository

---
+++ src/test/java/AppClass_Test.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class AppClass_Test {
+
+    @Test
+    public void test1(){
+        System.out.println("running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("running test 2");
+    }
+
+}
